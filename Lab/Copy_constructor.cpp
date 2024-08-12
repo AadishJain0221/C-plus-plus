b3e6ee99ceab9a6300db5c398528efd5ccd41b7d
AadishJain0221 <163055501+AadishJain0221@users.noreply.github.com>
2024-08-12 16:36:54 +0530

    created class complex

--- Lab/Copy_constructor.cpp
+++ Lab/Copy_constructor.cpp
@@ -1,3 +1,14 @@
 // This program is to illustrate the working of copy constructor
 #include<iostream>
 using namespace std;
+
+class complex 
+{
+  int a,b;
+  public:
+  complex(int a1=0,int b1=0)
+ {
+   a=a1;
+   b=b1;
+ }
+}
