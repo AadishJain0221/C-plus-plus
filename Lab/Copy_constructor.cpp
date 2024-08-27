339612d42c5577571844b0c8594a852e10e88cad
AadishJain0221 <aadishbaphana@gmail.com>
2024-08-27 19:17:00 +0530

    completed copy constructor program

--- Lab/Copy_constructor.cpp
+++ Lab/Copy_constructor.cpp
@@ -1,14 +1,42 @@
 // This program is to illustrate the working of copy constructor
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class complex 
+class complex
 {
-  int a,b;
-  public:
-  complex(int a1=0,int b1=0)
- {
-   a=a1;
-   b=b1;
- }
+    int a, b;
+
+public:
+    complex(int a1 = 0, int b1 = 0)
+    {
+        a = a1;
+        b = b1;
+    }
+    complex(complex &p)
+    {
+        cout<<"\nCopy Constructor\n";
+        a=p.a;
+        b=p.b;
+    }
+    void putdata()
+    {
+        cout<<a<<"+ i"<<b<<endl;
+    }
+
+};
+
+int main()
+{
+    complex c1(3,4);
+    c1.putdata();
+
+    complex c2=c1;
+    c2.putdata();
+
+    // complex c2(c1);   another way to call copy constructor
+
+    // complex c2;
+    // c2=c1;    invalid syntax to call copy constructor
+
+    return 0;
 }
